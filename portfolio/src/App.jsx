7a101c2e66603be0b2f72638ccbe8e6c6e6ee939
SihakV <ssam1@paragoniu.edu.kh>
2024-09-18 17:30:16 +0700

    finished hero

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import AboutMe from "./components/AboutMe";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <AboutMe />
     </>
   );
 }
